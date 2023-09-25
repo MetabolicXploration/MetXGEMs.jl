08fdc4b7158b89a33c13cb4880cbefdc8b8ea68c
josePereiro <jose.pereiro.personal@gmail.com>
2023-09-25 19:16:38 -0400

    new rxn_str(...; full)

--- src/MetNetUtils/rxn_str.jl
+++ src/MetNetUtils/rxn_str.jl
@@ -1,17 +1,37 @@
-function rxn_str(net::MetNet, ider)
+function rxn_str(net::MetNet, ider; full = false)
     ridx = rxnindex(net, ider)
     arrow_str = isblocked(net, ridx) ? " >< " : 
                 isbkwd_bounded(net, ridx) ? " <== " :
                 isfwd_bounded(net, ridx) ? " ==> " : " <==> " 
     
-    reacts = rxn_reacts(net, ridx)
-    react_str = join([string("(", stoi(net, react, ridx), ") ", 
-        metabolites(net, react))  for react in reacts], " + ")
-    
-    prods = rxn_prods(net, ridx)
-    prods_str = join([string("(", stoi(net, prod, ridx), ") ", 
-        metabolites(net, prod))  for prod in prods], " + ")
-    return react_str * arrow_str * prods_str
+    react_str = String[]
+    for meti in rxn_reacts(net, ridx)
+        s = stoi(net, meti, ridx)
+        id = metabolites(net, meti)
+        name = _getindex_or_nothing(net.metNames, meti)
+        name = isnothing(name) ? "" : name
+        push!(react_str, 
+            full ? 
+                string("(", s, ") ", id, "::[", name, "]") :
+                string("(", s, ") ", id)
+        )
+    end
+    react_str = join(react_str, " + ")
+
+    prods_str = String[]
+    for meti in rxn_prods(net, ridx)
+        s = stoi(net, meti, ridx)
+        id = metabolites(net, meti)
+        name = _getindex_or_nothing(net.metNames, meti)
+        name = isnothing(name) ? "" : name
+        push!(prods_str, 
+            full ? 
+                string("(", s, ") ", id, "::[", name, "]") :
+                string("(", s, ") ", id)
+        )
+    end
+    prods_str = join(prods_str, " + ")
+    return string(react_str,  arrow_str,  prods_str)
 end
-rxn_str(net::MetNet, iders::Vector) = [rxn_str(net, ider) for ider in iders]
-rxn_str(net::MetNet) = rxn_str(net, net.rxns)
+rxn_str(net::MetNet, iders::Vector; full = false) = [rxn_str(net, ider; full) for ider in iders]
+rxn_str(net::MetNet; full = false) = rxn_str(net, net.rxns; full)
